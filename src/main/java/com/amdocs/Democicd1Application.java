77adf08b53f837f17350c27a5b0cea560da5dfea
Shubham <sy372000@gmail.com>
2023-10-20 11:56:31 +0530

    first

---
+++ src/main/java/com/amdocs/Democicd1Application.java
@@ -0,0 +1,13 @@
+package com.amdocs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Democicd1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Democicd1Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/amdocs/app1.java
@@ -0,0 +1,8 @@
+package com.amdocs;
+
+public class app1 {
+	public static void main(String[] args) {
+		System.out.println("hello world");
+	}
+
+}
